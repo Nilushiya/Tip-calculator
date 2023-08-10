1601d7167ffc9d2409a2656cf04a3ffe27d692bc
Nilushiya <nilushiyak@gmail.com>
2023-08-10 09:51:01 +0530

    basics

---
+++ script.js
@@ -0,0 +1,9 @@
+//const bill = document.querySelector("bill");
+//let npeople = document.getElementById("npeople");
+//let defaultBox = document.getElementById("defaultBox");
+//let tip = document.getElementById("tipAmount");
+let total = document.getElementById("totalAmount");
+
+let bill = Number(document.getElementById("bill").value)
+total.innerHTML = "Jkk"
+

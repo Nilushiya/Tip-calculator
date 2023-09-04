7bc196dc21ccf25978d4f44566292fdf7aaf7004
Nilushiya <nilushiyak@gmail.com>
2023-09-05 03:44:24 +0530

    input validation

--- script.js
+++ script.js
@@ -9,9 +9,16 @@ let box4 = document.querySelector(".box4")
 let box5 = document.querySelector(".box5")
 let defaultBox = document.querySelector(".defaultBox");
 let reset = document.querySelector(".reset")
+let errormsg =document.querySelector(".errormsg")
 
 
 box1.addEventListener("click",() => {
+   // errormsg.innerHTML = "'sorry'"
+   if(Number(input.value) === 0){
+    errormsg.innerHTML = "'sorry'"
+    inputValidate()
+   }
+   else{
     if(Number(numberOfPeople.value) === 0){
         validate()
     }
@@ -19,12 +26,16 @@ box1.addEventListener("click",() => {
         tipAmount.innerHTML = " $ "+ ((Number(input.value)*0.05)/Number(numberOfPeople.value)).toFixed(2)
         totalAmount.innerHTML = " $ "+ ((Number(input.value)*1.05)/Number(numberOfPeople.value)).toFixed(2)
     }
+   }
 })
 
 box2.addEventListener("click",() => {
     if(Number(numberOfPeople.value) === 0){
         validate()
     }
+  //  else if(Number(numberOfPeople.value) === null){
+  //      validatee()
+  //  }
     else{
     tipAmount.innerHTML = " $ "+ ((Number(input.value)*0.1)/Number(numberOfPeople.value)).toFixed(2)
     totalAmount.innerHTML = " $ "+ ((Number(input.value)*1.1)/Number(numberOfPeople.value)).toFixed(2)
@@ -70,26 +81,47 @@ defaultBox.addEventListener('keypress',function (defaul){
         totalAmount.innerHTML = " $ "+ ((Number(input.value)*(1+(Number(defaultBox.value)/100)))/Number(numberOfPeople.value)).toFixed(2)
     }}
 })
-
+function validate(){
+    setError(numberOfPeople);
+}
+let can = document.querySelector(".cannot")
+function setError(element){
+    element.style.color="red"
+    element.style.borderColor = "red"
+    
+    can.style.color = "red"
+ 
+    tipAmount.innerHTML = " $ " + 0.00.toFixed(2)
+    totalAmount.innerHTML = " $ "+ 0.00.toFixed(2)
+}
 reset.addEventListener("click",() => {
     input.value = ''
     numberOfPeople.value = ''
     defaultBox.value = ''
     tipAmount.innerHTML = ""
     totalAmount.innerHTML = ""
+    numberOfPeople.style.borderColor = "black"
+    can.style.color = "black"
+    input.style.borderColor = 'black'
+    errormsg.style.color = 'black'
+
 })
-
-function validate(){
-    setError(numberOfPeople);
+function inputValidate(){
+    errormsg.innerHTML = "can't be Zero"
+    input.style.borderColor = 'red'
+    //input.style.Color = 'red'
+    errormsg.style.color = 'red'
 }
-function setError(element){
-    element.style.color="red"
-    element.style.borderColor = "red"
-    let can = document.querySelector(".cannot")
-    can.style.color = "red"
 
+/*function validatee(){
+    setErrorr(numberOfPeople);
+}
+function setErrorr(element){
+    element.style.color="green"
+    element.style.borderColor = "green"
+    
+    can.style.color = "green"
+ 
     tipAmount.innerHTML = " $ " + 0.00.toFixed(2)
     totalAmount.innerHTML = " $ "+ 0.00.toFixed(2)
-}
-
-
+}*/
